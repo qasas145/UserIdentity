0f0d52b2bdaf7107c80c3645a414a18e3d32e486
qasas145 <mohamed.sayed14527@yahoo.com>
2024-11-05 22:04:26 +0200

    adding username field

--- UserIdentity/ViewData/UserViewModel.cs
+++ UserIdentity/ViewData/UserViewModel.cs
@@ -2,5 +2,6 @@ public class UserViewModel {
     public string Id{get;set;}
     public string FirstName{get;set;}
     public string LastName{get;set;}
+    public string UserName{get;set;}
     public IEnumerable<string> Roles{get;set;}
 }
