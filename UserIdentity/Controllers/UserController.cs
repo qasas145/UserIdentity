02a7c8fae2383347d77f8a4ff77e3a170e404c26
qasas145 <mohamed.sayed14527@yahoo.com>
2024-11-05 22:01:36 +0200

    Adding actions for adding Users by admin

--- UserIdentity/Controllers/UserController.cs
+++ UserIdentity/Controllers/UserController.cs
@@ -20,6 +20,7 @@ public class UserController : Controller {
                 Id = user.Id,
                 FirstName = user.FirstName,
                 LastName = user.LastName,
+                UserName = user.UserName,
                 Roles = _userManager.GetRolesAsync(user).Result
             
         });
@@ -31,20 +32,24 @@ public class UserController : Controller {
 
         var user = await _userManager.FindByIdAsync(userId);
         var userRoles = await _userManager.GetRolesAsync(user);
-
         var roles = await _roleManager.Roles.ToListAsync();
-        var filteredRoles = roles.Where(r=>!userRoles.Contains(r.Name)).Select(r=>r.Name);
+
+        var filteredRoles = roles.Where(r=>!userRoles.Contains(r.Name)).Select(r=>new RoleFormModel(){
+            Name = r.Name,
+            IsSelected = false
+        }).ToList();
 
         var model = new RoleUserFormModel(){Roles = filteredRoles, UserId = userId};
         
         return View(model);
     }
     [HttpPost]
-    public async Task<IActionResult> AddRoleToUser(RoleUserFormModel RoleUserFormModel) {
+    public async Task<IActionResult> AddRoleToUser(RoleUserFormModel model) {
 
-        var user = await _userManager.FindByIdAsync(RoleUserFormModel.UserId.ToString());
+        var user = await _userManager.FindByIdAsync(model.UserId.ToString());
+        var selectedRoles = model.Roles.Where(r=>r.IsSelected).Select(r=>r.Name);
 
-        await _userManager.AddToRoleAsync(user, RoleUserFormModel.Role);
+        await _userManager.AddToRolesAsync(user, selectedRoles);
 
         return RedirectToAction(nameof(Index));
     }
@@ -56,16 +61,61 @@ public class UserController : Controller {
         var user = await _userManager.FindByIdAsync(userId);
         var userRoles = await _userManager.GetRolesAsync(user);
 
-        var model = new RoleUserFormModel(){Roles = userRoles, UserId = userId};
+        var model = new RoleUserFormModel(){Roles = userRoles.Select(r=>new RoleFormModel(){
+            Name = r,
+            IsSelected = false
+        }).ToList(), UserId = userId};
         
         return View(model);
     }
     [HttpPost]
-    public async Task<IActionResult> DeleteRoleFromUser(RoleUserFormModel RoleUserFormModel) {
+    public async Task<IActionResult> DeleteRoleFromUser(RoleUserFormModel model) {
         
-        var user = await _userManager.FindByIdAsync(RoleUserFormModel.UserId);
-        await _userManager.RemoveFromRoleAsync(user, RoleUserFormModel.Role);
+        var user = await _userManager.FindByIdAsync(model.UserId);
+        await _userManager.RemoveFromRolesAsync(user, model.Roles.Where(r=>r.IsSelected).Select(r=>r.Name));
         
+        return RedirectToAction(nameof(Index));
+    }
+    public async Task<IActionResult> AddUser() {
+        var roles = await _roleManager.Roles.Select(r=>new RoleFormModel(){IsSelected = false,Name = r.Name}).ToListAsync();
+        var newUser = new AddUserFormModel() {
+            Roles = roles
+        };
+        return View(newUser);
+    }
+    [HttpPost]
+    public async Task<IActionResult> AddUser(AddUserFormModel model) {
+        
+        if (!ModelState.IsValid) {
+            return View(model);
+        }
+        else if (model.Roles.Where(r=>r.IsSelected).Count() == 0)    {
+            ModelState.AddModelError("Roles", "You must select at least one role");
+        }
+        else if (await _userManager.FindByEmailAsync(model.Email) != null) {
+            ModelState.AddModelError("Email", "The email exists already");
+        }
+
+        var user = new ApplicationUser() {
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            Email = model.Email,
+            UserName = model.UserName
+        };
+
+
+        var result = await _userManager.CreateAsync(user,model.Password);
+
+        if (result.Succeeded) {
+            await _userManager.AddToRolesAsync(user, model.Roles.Where(r=>r.IsSelected).Select(r=>r.Name).ToList());
+        }
+        else {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("Roles", error.Description);
+            }
+            return View(model);
+        }
         return RedirectToAction(nameof(Index));
     }
 }
