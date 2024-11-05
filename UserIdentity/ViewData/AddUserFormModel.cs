203b8639854bc499e592616d4a58f844e7dbb6e8
qasas145 <mohamed.sayed14527@yahoo.com>
2024-11-05 22:00:56 +0200

    Added viewData and views for adding user by admin

---
+++ UserIdentity/ViewData/AddUserFormModel.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel.DataAnnotations;
+
+public class AddUserFormModel {
+    
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            
+            [Required]
+            [Display(Name = "First Name ")]
+            public string FirstName{get;set;}
+            [Required]
+            [Display(Name = "Last Name ")]
+            public string LastName{get;set;}
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [Display(Name = "UserName")]
+            public string UserName{get;set;}
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            public List<RoleFormModel> Roles{get;set;}
+}
